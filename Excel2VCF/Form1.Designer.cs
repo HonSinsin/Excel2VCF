c272b1651e8db5b07c768bf4368204336d6cc567
Honsinsin <fx130903@163.com>
2019-03-21 20:01:14 +0800

    Initial submit

---
+++ Excel2VCF/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Excel2VCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsel = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnsel
+            // 
+            this.btnsel.Location = new System.Drawing.Point(31, 43);
+            this.btnsel.Name = "btnsel";
+            this.btnsel.Size = new System.Drawing.Size(97, 23);
+            this.btnsel.TabIndex = 0;
+            this.btnsel.Text = "选择文件(&A)";
+            this.btnsel.UseVisualStyleBackColor = true;
+            this.btnsel.Click += new System.EventHandler(this.btnsel_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFilePath.Location = new System.Drawing.Point(149, 43);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(204, 23);
+            this.lblFilePath.TabIndex = 1;
+            this.lblFilePath.Text = "        ";
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(31, 91);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(97, 23);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "保存文件(&S)";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(443, 379);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 599);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnsel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel2VCF";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsel;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Excel2VCF/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataTable = System.Data.DataTable;
+
+namespace Excel2VCF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Event
+
+        private void btnsel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "选择电话本";
+            op.Multiselect = false;
+            op.Filter = "Excel文件|*.xlsx|Excel文件|*.xls";
+            if (DialogResult.OK == op.ShowDialog())
+            {
+                lblFilePath.Text = op.FileName;
+                dataGridView1.DataSource = GetExcelTable();
+                //dataGridView1.Enabled = false;
+            }
+        }
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Title = "保存为VCF文件";
+            sf.Filter = "vcf文件|*.vcf";
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                SaveVCF(sf.FileName);
+            }
+        }
+        #endregion
+
+        #region Method
+
+        /// <summary>
+        /// 读取excel表
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetExcelTable()
+        {
+            string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + lblFilePath.Text.Trim() + ";Extended Properties='Excel 12.0; HDR=Yes; IMEX=1'";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            DataTable schemaTable = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, null);// 获取Sheet列表
+            DataSet ds = new DataSet();
+            OleDbDataAdapter adp;
+            foreach (DataRow item in schemaTable.Rows)
+            {
+                adp = new OleDbDataAdapter($"Select * from [{item["TABLE_NAME"]}]", conn);
+                adp.Fill(ds);
+            }
+
+            return ds.Tables[0];
+        }
+
+        void SaveVCF(string path)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (DataGridViewRow item in dataGridView1.Rows)
+                {
+                    if (item.Cells[0].Value == null)
+                    {
+                        break;
+                    }
+                    string name = item.Cells[0].Value.ToString();
+                    string nums = item.Cells[1].Value.ToString();
+                    sb.AppendLine("BEGIN:VCARD");
+                    sb.AppendLine("VERSION:3.0");
+                    sb.AppendLine($"N:{name.Substring(0, name.Length / 2)};{name.Substring(name.Length / 2)};;;");
+                    sb.AppendLine($"FN:{name}");
+                    sb.AppendLine($"TEL;TYPE=CELL:{nums}");
+                    sb.AppendLine("END:VCARD");
+
+                }
+                File.WriteAllText(path, sb.ToString());
+                MessageBox.Show(this, "转换保存成功！！", "提示", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "转换保存失败！！", "提示", MessageBoxButtons.OK);
+                WriteLog(ex);
+            }
+        }
+
+        public static void WriteLog(Exception ex)
+        {
+            string logpath = Path.Combine(Application.StartupPath, "errlog.txt");
+            StreamWriter sw = File.AppendText(logpath);
+            sw.WriteLine();
+            sw.WriteLine(string.Format("{0}Error 时间:{1}{0}", "---------------------------------", DateTime.Now));
+            sw.WriteLine("导致错误信息 : " + ex.Message);
+            sw.WriteLine(string.Format("导致错误的方法 :{0}.{1}", ex.TargetSite.DeclaringType.FullName, ex.TargetSite.Name));
+            sw.WriteLine();
+            sw.WriteLine(ex.StackTrace.ToString());
+            //sw.WriteLine();
+            //sw.WriteLine(ex.ToString());
+            sw.Flush();
+            sw.Close();
+        }
+        #endregion
+
+    }
+}
